7ffc75c355c465b4be873b87ee6b8cb8a226d4b2
Sunset-wkshp <amber37@mail.usf.edu>
2019-01-21 15:25:32 -0500

    Intial Commit
    (Modified) Sample Code and encoder class

---
+++ encoder_class.py
@@ -0,0 +1,81 @@
+# encoder_class.py
+# This program demonstrates usage of the digital encoders.
+# After executing the program, manually spin the wheels and observe the output.
+# See https://sourceforge.net/p/raspberry-gpio-python/wiki/Inputs/ for more details.
+
+import time
+import RPi.GPIO as GPIO
+import signal
+
+class Encoder():
+    step_count = ()
+    LENCODER = 17
+    RENCODER = 18
+
+    def __init__(self):
+        self.step_count = (0,0)
+        # Attach the Ctrl+C signal interrupt
+        signal.signal(signal.SIGINT, self.ctrlC)
+
+        # Set the pin numbering scheme to the numbering shown on the robot itself.
+        GPIO.setmode(GPIO.BCM)
+
+        # Set encoder pins as input
+        # Also enable pull-up resistors on the encoder pins
+        # This ensures a clean 0V and 3.3V is always outputted from the encoders.
+        GPIO.setup(self.LENCODER, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.RENCODER, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+        # Attach a rising edge interrupt to the encoder pins
+        GPIO.add_event_detect(self.LENCODER, GPIO.RISING, self.onLeftEncode)
+        GPIO.add_event_detect(self.RENCODER, GPIO.RISING, self.onRightEncode)
+
+    # Pins that the encoders are connected to
+
+
+    # This function is called when the left encoder detects a rising edge signal.
+    def onLeftEncode(self , pin):
+        print("Left encoder ticked!")
+        self.step_count = (self.step_count[0]+1, self.step_count[1])
+
+    # This function is called when the right encoder detects a rising edge signal.
+    def onRightEncode(self, pin):
+        print("Right encoder ticked!")
+        # global step_count
+        self.step_count = (self.step_count[0], self.step_count[1]+1)
+
+    # This function is called when Ctrl+C is pressed.
+    # It's intended for properly exiting the program.
+    def ctrlC(self, signum, frame):
+        print("Exiting")
+        GPIO.cleanup()
+        exit()
+
+    # Initalizes Encoders
+    # Is this needed?
+    def initEncoders(self):
+        self.step_count = (0,0)
+
+    def resetCounts(self):
+        self.step_count = (0,0)
+
+    # Returns left and right ticks from the encoders
+    # (left count, right count)
+    def getCounts(self):
+        print(self.step_count)
+    # returns speed of left and right wheels
+    # (left speed, right speed)
+    def getSpeeds(self):
+        #get time
+        #get counts
+        #wait
+        #get time again
+        #get counts 
+
+
+## Main program
+if __name__ == "__main__":
+    d = Encoder()
+    while True:
+        time.sleep(1)
+        d.getCounts()
