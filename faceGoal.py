43b8bd3d8a0cdda553f809da3bd710a32ac2363a
robit <username@users.noreply.github.com>
2019-03-23 17:36:32 -0400

    from robot

--- faceGoal.py
+++ faceGoal.py
@@ -19,12 +19,13 @@ def faceGoal(state_machine):
     rob = Robot()
     max_forward = rob.encoder.get_max_forward_speed()
     max_backward = rob.encoder.get_max_backward_speed()
+    ERROR = 0.2
 
     desired_distance = 5
     # Proportional gain
     Kp = 0.01
 
-    user_input = input("Place robot facing goal and press enter to continue.")
+    user_input = input("Press enter to continue.")
 
     while (True):
         blobs = rob.camera.get_blobs()
@@ -39,11 +40,16 @@ def faceGoal(state_machine):
 
         if len(blobs) > 0:
             proportional_control = saturation_function(Kp * (blobs[largest].pt[0] - 320),
-                                                        max_forward, max_backward, 0.2)
+                                                        max_forward, max_backward, ERROR)
         else:
-            proportional_control = saturation_function(Kp * 640, max_forward, max_backward, 0.2)
+            proportional_control = saturation_function(Kp * 640, max_forward, max_backward, ERROR)
 
-        rob.encoder.setSpeedsIPS(proportional_control, -proportional_control)
+        # exit if used in a state machine and error is within acceptable range
+        if state_machine and ((Kp * (blobs[largest].pt[0] - 320)) > -error) and ((Kp * (blobs[largest].pt[0] - 320)) < error):
+            return
+        else:
+            rob.encoder.setSpeedsIPS(proportional_control, -proportional_control)
+
         time.sleep(0.01)
 
 if __name__ == "__main__":
